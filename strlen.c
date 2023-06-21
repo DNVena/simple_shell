b7fd60d4b78a5fbfa10b26f755a097243747f888
mreabetswe <reabetswemokoto123@gmail.com>
2023-06-21 16:19:53 -0700

    strlen.c

---
+++ strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - finds the length of a string
+ * @str: takes a character pointer
+ * Return: Always 0
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (0);
+}
