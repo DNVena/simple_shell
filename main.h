a21a38592a48d2e24b3da7692c70770ae1b3f284
Dorcas <dnmohlala@gmail.com>
2023-06-28 10:01:32 -0700

    header file

--- main.h
+++ main.h
@@ -1,9 +1,22 @@
 #ifndef main_h
 #define main_h
-char *_strdup(char *str)
-char *_strcpy(char *dest, char *src);
+extern char **environ;
+
 int _strlen(char *str);
-int _len(char *buffer);
-void _exec(char **args);
-char **parse(char *buf_cpy, char *buffer);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+char *_strncat(char *dest, char *src, int n);
+void _putchar(char c);
+int len_buf(char *buffer);
+char **tokenize(char *buffer);
+char **sep_path(char **envp);
+int len_path(char *buffer);
+char *_getenv(char **envp);
+char **checker(char **path, char **args);
+char *find_path(char **path, char *filename);
+char **prep(char *buffer, char **path);
+void _exec(char **args, char **envp);
+char *command(char **path, char *filename);
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, int n);
 #endif
